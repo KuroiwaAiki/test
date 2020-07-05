62e67166c0742ec6daf7c52cecb242bc7f3ecb24
Kuroiwa Aiki <63276107+KuroiwaAiki@users.noreply.github.com>
2020-07-05 00:42:01 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+const packs = [
+            {
+                imgURL: "https://surgesounds.com/wp-content/uploads/SurgeSounds-FutureBounce2Promo4.jpg",
+                title: "FUTURE BOUNCE 2",
+                subTitle: "Inspired by Brooks Mesto &amp; Mike Williams",
+                description: "128 Serum Presets 3 Construction Kits",
+                price: "$29"
+            },
+        ]
 
+        function Pack(){
+            return (
+                <div>
+                    {packs.map(pack => 
+                        <a>
+                            <img src={pack.imgURL}/>
+                            <h2>{pack.title}</h2>
+                            <p>{pack.subTitle}</p>
+                            <p>{pack.description}</p>
+                            <p>{pack.price}</p>
+                        </a>
+                    )}
+                </div>
+            )
+        }
+
+        ReactDOM.render(
+                <Pack/>,
+            document.getElementById('root')
+        );
